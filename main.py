7aa028951411f8e298d8b3343c7e3edf5bae256c
Fabricio Gatto <fa_gatto7@yahoo.com.br>
2022-09-14 18:20:47 -0300

    alteração do openapi

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 # direitos reservados.
 import uvicorn
 from fastapi import FastAPI
+from fastapi.openapi.utils import get_openapi
 from starlette.middleware.cors import CORSMiddleware
 from starlette.staticfiles import StaticFiles
 
@@ -21,10 +22,10 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:8000"],
-    allow_credentials=False, # se vai aceitar cookies como credenciais
-    allow_methods=["*"], # métodos que o request pode apresentar para o backend
-    allow_headers=["*"], # atributos do headers que o request pode apresentar para o backend
-    expose_headers=["Authorization"] # atributos do headers que o browser pode acessar do response
+    allow_credentials=False,  # se vai aceitar cookies como credenciais
+    allow_methods=["*"],  # métodos que o request pode apresentar para o backend
+    allow_headers=["*"],  # atributos do headers que o request pode apresentar para o backend
+    expose_headers=["Authorization"]  # atributos do headers que o browser pode acessar do response
 )
 
 app.mount("/estaticos", StaticFiles(directory="estaticos"), name="estaticos")
@@ -42,9 +43,28 @@ app.add_event_handler("startup", criar_tabelas)
 app.add_exception_handler(InvalidIdException, invalid_id)
 
 
+def custom_openapi():
+    if app.openapi_schema:
+        return app.openapi_schema
+    openapi_schema = get_openapi(
+        title="Hexagoon (based on FastApi)",
+        version="0.0.1",
+        routes=app.routes,
+    )
+    # openapi_schema["info"]["x-logo"] = {
+    #     "url": "https://fastapi.tiangolo.com/img/logo-margin/logo-teal.png"
+    # }
+    app.openapi_schema = openapi_schema
+    return app.openapi_schema
+
+
+app.openapi = custom_openapi
+
+
 @app.get("/")
 async def hello_world():
     return {"resultado": "Hello World"}
+
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
